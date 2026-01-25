0c4e1a09ab30c4cd8d9baaa7c31e049d9e4a4eec
VersanMihai <versan.mihai@gmail.com>
2026-01-25 13:23:41 +0200

    Restructure project to multi-module Gradle setup
    Moved application code to 'app' module and added new modules: user, chat, notification, and common. Updated build scripts to support a multi-module structure and adjusted settings.gradle.kts to include all modules. This refactor prepares the project for modular development and better separation of concerns.

---
+++ spring/chirp/common/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dependency.management)
+    alias(libs.plugins.kotlin.jpa)
+}
+
+group = "com.mversan"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
