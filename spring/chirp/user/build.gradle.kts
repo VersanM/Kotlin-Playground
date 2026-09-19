77b368b17685531245419a8b42cc34e620e96a4e
VersanMihai <versan.mihai@gmail.com>
2026-09-19 17:21:16 +0300

    added password hash support

--- spring/chirp/user/build.gradle.kts
+++ spring/chirp/user/build.gradle.kts
@@ -18,6 +18,8 @@ kotlin {
 dependencies {
     implementation(projects.common)
 
+    implementation(libs.spring.boot.starter.security)
+
     implementation(libs.spring.boot.starter.data.jpa)
     runtimeOnly(libs.postgresql)
 
