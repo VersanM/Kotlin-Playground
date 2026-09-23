33713343c2450e55cf1e23e95784c7c12e62e62f
VersanMihai <versan.mihai@gmail.com>
2026-09-23 19:37:06 +0300

    added login functionality

---
+++ spring/chirp/user/src/main/kotlin/com/stavrom/chirp/domain/exception/UserNotFoundException.kt
@@ -0,0 +1,3 @@
+package com.stavrom.chirp.domain.exception
+
+class UserNotFoundException : RuntimeException("User not found")
--- spring/chirp/user/src/main/kotlin/com/stavrom/chirp/service/auth/AuthService.kt
+++ spring/chirp/user/src/main/kotlin/com/stavrom/chirp/service/auth/AuthService.kt
@@ -1,17 +1,28 @@
 package com.stavrom.chirp.service.auth
 
+import com.stavrom.chirp.domain.exception.InvalidCredentialsException
 import com.stavrom.chirp.domain.exception.UserAlreadyExistsException
+import com.stavrom.chirp.domain.exception.UserNotFoundException
+import com.stavrom.chirp.domain.model.AuthenticatedUser
 import com.stavrom.chirp.domain.model.User
+import com.stavrom.chirp.domain.model.UserId
+import com.stavrom.chirp.infra.database.entities.RefreshTokenEntity
 import com.stavrom.chirp.infra.database.entities.UserEntity
 import com.stavrom.chirp.infra.database.mappers.toUser
+import com.stavrom.chirp.infra.database.repositories.RefreshTokenRepository
 import com.stavrom.chirp.infra.database.repositories.UserRepository
 import com.stavrom.chirp.infra.security.PasswordEncoder
 import org.springframework.stereotype.Service
+import java.security.MessageDigest
+import java.time.Instant
+import java.util.*
 
 @Service
 class AuthService(
     private val userRepository: UserRepository,
-    private val passwordEncoder: PasswordEncoder
+    private val passwordEncoder: PasswordEncoder,
+    private val jwtService: JwtService,
+    private val refreshTokenRepository: RefreshTokenRepository
 ) {
 
     fun register(email: String, username: String, password: String): User {
@@ -30,5 +41,51 @@ class AuthService(
         ).toUser()
 
         return savedUser
+    }
+
+    fun login(
+        email: String,
+        password: String
+    ): AuthenticatedUser {
+        val user = userRepository.findByEmail(email.trim()) ?: throw InvalidCredentialsException()
+
+        if (!passwordEncoder.matches(password, user.hashedPassword)) {
+            throw InvalidCredentialsException()
+        }
+
+        //TODO: check for verified email
+
+        return user.id?.let { userId ->
+            val accessToken = jwtService.generateAccessToken(userId = userId)
+            val refreshToken = jwtService.generateRefreshToken(userId = userId)
+
+            storeRefreshToken(userId, refreshToken)
+
+            AuthenticatedUser(
+                user = user.toUser(),
+                accessToken = accessToken,
+                refreshToken = refreshToken
+            )
+        } ?: throw UserNotFoundException()
+    }
+
+    private fun storeRefreshToken(userId: UserId, token: String) {
+        val hashed = hashToken(token)
+        val expiryMs = jwtService.refreshTokenValidityMs
+        val expiresAt = Instant.now().plusMillis(expiryMs)
+
+        refreshTokenRepository.save(
+            RefreshTokenEntity(
+                userId = userId,
+                expiresAt = expiresAt,
+                hashedToken = hashed
+            )
+        )
+    }
+
+    private fun hashToken(token: String): String {
+        val digest = MessageDigest.getInstance("SHA-256")
+        val hashBytes = digest.digest(token.encodeToByteArray())
+        return Base64.getEncoder().encodeToString(hashBytes)
     }
 }
